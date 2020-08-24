57688ab133adab707086a34b497a84ea67bc2ce4
Erick Yondon <eryondon@microsoft.com>
2020-08-23 22:27:39 -0700

    CoParent added. Same as ConsoleApp2.

--- MainApplication.Xplat/Program.cs
+++ MainApplication.Xplat/Program.cs
@@ -4,6 +4,7 @@ namespace MainApplication.Xplat
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
---
+++ TClassLibrary1/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TClassLibrary1
+{
+    public class Class1
+    {
+    }
+}
